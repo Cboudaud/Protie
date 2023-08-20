76b72f9c4017902ed798fa0b03f4aa35c1ff502b
Boudaud Camille <Camille@macbook-pro.home>
2023-08-21 01:51:16 +0200

    redux installed + data file finished + modifications on the inputs

---
+++ protie/src/actions/calculator.js
@@ -0,0 +1,9 @@
+// Action type
+export const TYPE_VEGGIE = 'TYPE_VEGGIE';
+
+
+// Action creator
+export const typeVeggie = (value) => ({
+  type: TYPE_VEGGIE,
+  input: value,
+});
--- protie/src/components/Calculator/Calculator.jsx
+++ protie/src/components/Calculator/Calculator.jsx
@@ -1,12 +1,22 @@
 import './styles.scss'
 import salad from '../../assets/healthy-food-salad-svgrepo-com.svg'
+import { useDispatch, useSelector } from 'react-redux';
+import { typeVeggie } from '../../actions/calculator';
 
 
 export default function Calculator() {
+  const { veggieInput } = useSelector((state) => state)
+  const dispatch = useDispatch();
 
   return (
 <div className="input-group">
-    <input className='input-group_food' placeholder='L&#39;aliment que vous avez consommé'/>
+    <input 
+      className='input-group_food' 
+      placeholder='L&#39;aliment que vous avez consommé'
+      value={veggieInput}
+      onChange={(e) => dispatch(typeVeggie(e.target.value))}
+
+    />
     <div className='input-group_quantity'>
       <input className='input-group_quantity_number' placeholder='La quantité'/><span>/grs</span>
     </div>
